d20fa7b616fd4da9a4ff371b5b26a43c8f6d7b59
sachin_lale <sachin_lale>
2009-10-28 15:21:19 +0000

    Integrated new query and DE
    SVN-Revision: 23313

--- WEB-INF/src/Test.java
+++ WEB-INF/src/Test.java
@@ -1,6 +1,32 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 
 public class Test
 {
 
+	public static void main(String[] args)
+	{
+		try
+		{
+			Date d = edu.wustl.common.util.Utility.parseDate("06-04-2009","dd-MM-yyyy");
+		System.out.println(d);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();		}
+	}
+	
+	public static String parseDateToString(Date date, String pattern)
+	{
+		String dateStr = "";
+		if (date != null)
+		{
+			SimpleDateFormat dateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+			dateStr = dateFormat.format(date);
+		}
+		return dateStr;
+	}
 }
