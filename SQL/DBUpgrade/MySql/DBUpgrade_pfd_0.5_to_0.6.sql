fc72aab58862e265ef4c9a91bd14812bb5da2735
sachin_lale <sachin_lale>
2009-10-28 12:31:57 +0000

    Commit
    SVN-Revision: 23303

--- SQL/DBUpgrade/MySql/DBUpgrade_pfd_0.5_to_0.6.sql
+++ SQL/DBUpgrade/MySql/DBUpgrade_pfd_0.5_to_0.6.sql
@@ -8,5 +8,3 @@ delete from CATISSUE_PERMISSIBLE_VALUE where identifier = 2641;
 delete from CATISSUE_PERMISSIBLE_VALUE where IDENTIFIER = 60;
 delete from CATISSUE_PERMISSIBLE_VALUE where IDENTIFIER = 2636;
 alter table catissue_address modify STREET text;
-Alter table CATISSUE_PARTICIPANT add (EMAIL_ADDRESS varchar(50));
-Alter table CATISSUE_PARTICIPANT add (BLOOD_GROUP varchar(20));
